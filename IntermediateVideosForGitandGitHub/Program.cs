2ddaa11168b81cf5c45141c125feccc0d3756c00
houghj16 <jehoughton@microsoft.com>
2023-10-19 09:44:44 -0700

    Add a new writeline statement

--- IntermediateVideosForGitandGitHub/Program.cs
+++ IntermediateVideosForGitandGitHub/Program.cs
@@ -1,2 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+Console.WriteLine("I'm excited to learn about Git and GitHub in Visual Studio!");
