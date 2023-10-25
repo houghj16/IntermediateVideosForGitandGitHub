7bd999c8ae85451b5e189df2be451843fdd50209
Jessie Houghton <46505805+houghj16@users.noreply.github.com>
2023-10-25 13:48:24 -0700

    Add new line

--- IntermediateVideosForGitandGitHub/Program.cs
+++ IntermediateVideosForGitandGitHub/Program.cs
@@ -5,6 +5,8 @@ Console.WriteLine("I'm excited to learn about Git and GitHub in Visual Studio!")
 
 Console.WriteLine("My first commit on the new feature branch");
 
+Console.WriteLine("My next commit on the new feature branch");
+
 Console.WriteLine("Execution finished here!"); 
 
 // Don't add this comment to the commit
