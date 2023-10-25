7068d4af1a975d74a2fb6519114a522ab39e50b8
Jessie Houghton <46505805+houghj16@users.noreply.github.com>
2023-10-25 13:56:08 -0700

    Revert

--- IntermediateVideosForGitandGitHub/Program.cs
+++ IntermediateVideosForGitandGitHub/Program.cs
@@ -5,10 +5,4 @@ Console.WriteLine("I'm excited to learn about Git and GitHub in Visual Studio!")
 
 Console.WriteLine("My first commit on the new feature branch");
 
-Console.WriteLine("My next commit on the new feature branch");
-
-Console.WriteLine("My next commit on the new feature branch");
-
-Console.WriteLine("Execution finished here!"); 
-
-// Don't add this comment to the commit
+Console.WriteLine("Execution finished here!"); // Don't add this comment to the commit
