3f8090e09eb8f01ee5734c93cc993658f22ca91d
Jessie Houghton <46505805+houghj16@users.noreply.github.com>
2023-10-25 13:54:42 -0700

    Commit these changes on the correct branch instead

--- IntermediateVideosForGitandGitHub/Program.cs
+++ IntermediateVideosForGitandGitHub/Program.cs
@@ -5,10 +5,17 @@ Console.WriteLine("I'm excited to learn about Git and GitHub in Visual Studio!")
 
 Console.WriteLine("My first commit on the new feature branch");
 
-Console.WriteLine("My next commit on the new feature branch");
+Console.WriteLine("Execution finished here!");
 
-Console.WriteLine("My next commit on the new feature branch");
+Console.WriteLine("Hello, World!");
 
-Console.WriteLine("Execution finished here!"); 
+Console.WriteLine("I'm excited to learn about Git and GitHub in Visual Studio!");
 
-// Don't add this comment to the commit
+Console.WriteLine("My first commit on the new feature branch");
+
+Console.WriteLine("Execution finished here!");
+
+// Remove this
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("New change");
